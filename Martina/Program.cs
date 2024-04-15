bd4a01b45c0ebd688b26d21e3039f8661f918068
jackfiled <xcrenchangjun@outlook.com>
2024-04-16 00:06:29 +0800

    feat: 自动重定向到index.html (#2)
    Reviewed-on: https://git.rrricardo.top/PostGuard/Martina/pulls/2

--- Martina/Program.cs
+++ Martina/Program.cs
@@ -1,5 +1,19 @@
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
 WebApplication application = builder.Build();
+
+if (application.Environment.IsDevelopment())
+{
+    application.UseSwagger();
+    application.UseSwaggerUI();
+}
+
+application.UseStaticFiles();
+
+application.MapControllers();
+application.MapFallbackToFile("/index.html");
 
 await application.RunAsync();
